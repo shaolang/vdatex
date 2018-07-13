46d3b0e399ddfe8b9de346cb30d02fc793359147
shaolang <shaolang@ymail.com>
2018-07-13 20:18:07 +0800

    Rename ValueDate as CurrencyDate

--- lib/vdatex/currency_date.ex
+++ lib/vdatex/currency_date.ex
@@ -1,11 +1,11 @@
-defmodule VDatex.ValueDate do
+defmodule VDatex.CurrencyDate do
   alias __MODULE__
 
   defstruct [:weekends]
 
-  def new(weekends \\ [6, 7]), do: %ValueDate{weekends: weekends}
+  def new(weekends \\ [6, 7]), do: %CurrencyDate{weekends: weekends}
 
-  def next_biz_day(%ValueDate{weekends: weekends} = vdate, curr_date) do
+  def next_biz_day(%CurrencyDate{weekends: weekends} = vdate, curr_date) do
     result = Date.add(curr_date, 1)
 
     if Date.day_of_week(result) in weekends do
--- test/currency_date_test.exs
+++ test/currency_date_test.exs
@@ -1,17 +1,17 @@
-defmodule VDatex.ValueDateTest do
+defmodule VDatex.CurrencyDateDate do
   use ExUnit.Case
   use ExUnitProperties
-  alias VDatex.ValueDate
+  alias VDatex.CurrencyDate
 
   property "next_biz_day/2 never falls on weekends" do
     check all day <- StreamData.integer(1..6),
               to_add <- StreamData.positive_integer() do
       weekends = [day, day + 1]
-      vdate = ValueDate.new(weekends)
+      vdate = CurrencyDate.new(weekends)
 
       curr_date = Date.new(2018, 1, 1) |> elem(1) |> Date.add(to_add)
 
-      refute Date.day_of_week(ValueDate.next_biz_day(vdate, curr_date)) in weekends
+      refute Date.day_of_week(CurrencyDate.next_biz_day(vdate, curr_date)) in weekends
     end
   end
 end
