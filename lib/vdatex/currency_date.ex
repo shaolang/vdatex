89a5f7f02a5f214898671610e3c602c817400ba2
shaolang <shaolang@ymail.com>
2018-07-13 22:36:16 +0800

    Add two properties to next_biz_day

--- lib/vdatex/currency_date.ex
+++ lib/vdatex/currency_date.ex
@@ -5,11 +5,15 @@ defmodule VDatex.CurrencyDate do
 
   def new(weekends \\ [6, 7]), do: %CurrencyDate{weekends: weekends}
 
-  def next_biz_day(%CurrencyDate{weekends: weekends} = vdate, curr_date) do
+  def next_biz_day(
+        %CurrencyDate{weekends: weekends} = vdate,
+        curr_date,
+        holidays \\ []
+      ) do
     result = Date.add(curr_date, 1)
 
-    if Date.day_of_week(result) in weekends do
-      next_biz_day(vdate, result)
+    if Date.day_of_week(result) in weekends or result in holidays do
+      next_biz_day(vdate, result, holidays)
     else
       result
     end
--- test/currency_date_test.exs
+++ test/currency_date_test.exs
@@ -4,12 +4,29 @@ defmodule VDatex.CurrencyDateDate do
   alias StreamData, as: SD
   alias VDatex.CurrencyDate
 
+  property "next_biz_day/2 is always one day after (assuming no weekends and holidays)" do
+    check all curr_date <- gen_date() do
+      cd = CurrencyDate.new([])
+
+      assert Date.diff(CurrencyDate.next_biz_day(cd, curr_date), curr_date) == 1
+    end
+  end
+
   property "next_biz_day/2 never falls on weekends" do
     check all weekends <- gen_weekends(),
               curr_date <- gen_date() do
-      vdate = CurrencyDate.new(weekends)
+      cd = CurrencyDate.new(weekends)
 
-      refute Date.day_of_week(CurrencyDate.next_biz_day(vdate, curr_date)) in weekends
+      refute Date.day_of_week(CurrencyDate.next_biz_day(cd, curr_date)) in weekends
+    end
+  end
+
+  property "next_biz_day/2 never falls on holidays" do
+    check all curr_date <- gen_date(),
+              holidays <- gen_holidays(curr_date) do
+      cd = CurrencyDate.new()
+
+      refute CurrencyDate.next_biz_day(cd, curr_date, holidays) in holidays
     end
   end
 
@@ -18,4 +35,12 @@ defmodule VDatex.CurrencyDateDate do
 
   defp gen_weekends(),
     do: SD.map(SD.integer(1..6), fn n -> [n, n + 1] end)
+
+  defp gen_holidays(curr_date) do
+    SD.positive_integer()
+    |> SD.list_of(min_length: 1)
+    |> SD.map(fn ns ->
+      Enum.map(MapSet.new(ns), fn n -> Date.add(curr_date, n) end)
+    end)
+  end
 end
