ffa35eaa7f76c1ad7c5805822e029b4ef2faf18b
shaolang <shaolang@ymail.com>
2018-07-13 21:33:47 +0800

    Create custom generators: gen_weekends and gen_date

--- test/currency_date_test.exs
+++ test/currency_date_test.exs
@@ -1,17 +1,21 @@
 defmodule VDatex.CurrencyDateDate do
   use ExUnit.Case
   use ExUnitProperties
+  alias StreamData, as: SD
   alias VDatex.CurrencyDate
 
   property "next_biz_day/2 never falls on weekends" do
-    check all day <- StreamData.integer(1..6),
-              to_add <- StreamData.positive_integer() do
-      weekends = [day, day + 1]
+    check all weekends <- gen_weekends(),
+              curr_date <- gen_date() do
       vdate = CurrencyDate.new(weekends)
-
-      curr_date = Date.new(2018, 1, 1) |> elem(1) |> Date.add(to_add)
 
       refute Date.day_of_week(CurrencyDate.next_biz_day(vdate, curr_date)) in weekends
     end
   end
+
+  defp gen_date(),
+    do: SD.map(SD.positive_integer(), fn n -> Date.add(~D[2018-01-01], n) end)
+
+  defp gen_weekends(),
+    do: SD.map(SD.integer(1..6), fn n -> [n, n + 1] end)
 end
